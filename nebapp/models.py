28cf361d2ffe6dd0dc7eda24438c2f13e99fef85
Joselyne97 <joselynejojo740@gmail.com>
2019-10-30 20:25:01 +0200

    Adding profile table

--- nebapp/models.py
+++ nebapp/models.py
@@ -4,6 +4,28 @@ import datetime as dt
 from tinymce.models import HTMLField
 
 
+class Profile(models.Model):
+
+    bio = HTMLField()
+    profile_pic = models.ImageField(upload_to = 'pic/', blank=True, null=True)
+    full_name = models.CharField(max_length=60)
+    user=models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    email=models.EmailField()
+    location = models.CharField(max_length =30,blank =True)
+
+    @classmethod
+    def update_profile(cls,id,value):
+        cls.objects.filter(id=id).update(user_id = new_user)
+
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return self.user
 
 
 
