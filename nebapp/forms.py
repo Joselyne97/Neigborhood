31b9a2541e1ea84b2ad5411dcab28bf2453b0bde
Joselyne97 <joselynejojo740@gmail.com>
2019-10-30 20:24:16 +0200

    Adding edit profile form

--- nebapp/forms.py
+++ nebapp/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from .models import Profile
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        exclude =['user']
