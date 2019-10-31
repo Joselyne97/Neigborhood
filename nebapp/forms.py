b2fafd511725e628e8de69f5bff82742c4efa295
Joselyne97 <joselynejojo740@gmail.com>
2019-10-31 20:01:05 +0200

    I added the profile form and the neigborhood form

--- nebapp/forms.py
+++ nebapp/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
-from .models import Profile
+from .models import Profile,Neighbourhood
 
 class NewProfileForm(forms.ModelForm):
     class Meta:
         model= Profile
+        fields = ['bio']
         exclude =['user']
+
+class HoodForm(forms.ModelForm):
+    class=Meta:
+        model= Neighbourhood
+
