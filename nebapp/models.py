f6b1a85b85c5b7da4ca5d98e51dd93de5fcf0ad7
Joselyne97 <joselynejojo740@gmail.com>
2019-11-01 11:05:48 +0200

    I added the class of join and post and business

--- nebapp/models.py
+++ nebapp/models.py
@@ -3,47 +3,37 @@ from django.contrib.auth.models import User
 import datetime as dt
 from tinymce.models import HTMLField
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Neighbourhood(models.Model):
-    places=(
-        ('Kicukiro','Kicukiro'),('Remera','Remera'),('Gatenga','Gatenga'),('Gisozi','Gisozi'),('Kacyiru','Kacyiru'),('Karuruma','Karuruma'),('Gatsata','Gatsata'),('Nyamirambo','Nyamirambo'),('Nyamata','Nyamata'),('Huye','Huye'),('Gisenyi','Gisenyi'),('Kibuye','Kibuye')
-    )
-    name=models.CharField(max_length=200)
+   
+    name=models.CharField(max_length=50)
     description=HTMLField()
     location=models.CharField(max_length=40, choices=places)
-    pub_date = models.DateTimeField(auto_now_add=True)
     police_contact=models.IntegerField(default='112')
     hospital_contact=models.IntegerField(default='911')
     user=models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def save_neighbourhood(self):
+    def save_hood(self):
         self.save()
 
-    def update_neighbourhood(self):
+    def update_hood(self):
         self.update()
 
-    def update_occupants(self):
-        self.update()
-
-
-    def delete_neighbourhood(self):
+    def delete_rhood(self):
         self.delete()
 
     
     @classmethod
-    def get_neighbourhoods(cls):
-        neighbourhoods=Neighbourhood.objects.objects.all()
-        return neighbourhoods
+    def get_hood_by_id(cls,id):
+        hood=Neighbourhood.objects.get(id=id)
+        return hood
 
-    @classmethod
-    def find_neighbourhood_by_id(cls,id):
-        neighbourhood=Neighbourhood.objects.get(id=id)
-        return neighbourhood
 
     @classmethod
     def search_by_title(cls,search_term):
-        neighbourhood=cls.objects.filter(name__icontains=search_term)
-        return neighbourhood
+        hood=cls.objects.filter(name__icontains=search_term)
+        return hood
 
     def __str__(self):
         return self.name
@@ -54,11 +44,13 @@ class Profile(models.Model):
 
     bio = HTMLField()
     user=models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    hood = models.OneToOneField(Neighbourhood, on_delete=models.CASCADE, blank =True,null=True, default='Kigali')
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,null=True, default='Kigali')
 
-    @classmethod
-    def update_profile(cls,id,value):
-        cls.objects.filter(id=id).update(user_id = new_user)
+    # @receiver(post_save, sender=User)
+    # def create_user_profile(sender, instance, **kwargs):
+    #     instance.profile.save()
+
+    # post_save.connect(save_user_profile, sender=User)
 
     
     def save_profile(self):
@@ -72,6 +64,99 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user
+
+    @classmethod
+    def get_user_by_hood(cls, id):
+        profile = Profile.objects.filter(hood_id=id).all()
+        return profile
+
+
+
+
+class Business(models.Model):
+    business_name=models.CharField(max_length=40)
+    business_description=HTMLField()
+    email=models.EmailField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    hood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE, null=True,blank=True)
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    def update_business(self):
+        self.update()
+
+    def __str__(self):
+        return self.business_name
+
+    @classmethod
+    def get_business_by_id(cls,id):
+        business=Business.objects.get(id=id)
+        return business
+
+    @classmethod
+    def get_businesses(cls,id):
+        businesses=Business.objects.filter(hood_id=id).all()
+        return businesses
+
+    # @classmethod
+    # def search_by_title(cls,search_term):
+    #     business = cls.objects.filter(title__icontains=search_term)
+    #     return business
+
+
+class Joining(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE )
+    hood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+    def save_joining(self):
+        self.save()
+
+    def delete_joining(self):
+        self.delete()
+
+    def update_joining(self):
+        self.update()
+
+
+class Post(models.Model):
+    topic=models.CharField(max_length=80)
+    post=HTMLField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    hood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.topic
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def update_post(self):
+        self.update()
+
+    @classmethod
+    def get_post_by_hood(cls,id):
+        post = Post.objects.filter(hood_id=id).all()
+        return post
+
+
+
+
+
+
+
+
+
 
 
 
