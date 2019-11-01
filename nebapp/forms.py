7fef5960cc5d8ccba2102fb39c77ac1c9011759a
Joselyne97 <joselynejojo740@gmail.com>
2019-11-01 11:04:43 +0200

    I added the forms

--- nebapp/forms.py
+++ nebapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
-from .models import Profile,Neighbourhood
+from .models import Profile,Neighbourhood,Post,Business
 
 class NewProfileForm(forms.ModelForm):
     class Meta:
@@ -10,6 +10,19 @@ class NewProfileForm(forms.ModelForm):
         exclude =['user']
 
 class HoodForm(forms.ModelForm):
-    class=Meta:
+    class Meta:
         model= Neighbourhood
+        fields = ['name','description','location','police_contact','hospital_contact']
+        exclude =['user']
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model= Business
+        exclude=['user','hood']
+
+class NewPostForm(form.ModelForm):
+    class Meta:
+        model= Post
+        exclude=['user','hood']
+
 
